68c3e4f0dd771201d4f74e1e8e1a07254b9ca0f5
wooopers <kngordon@msn.com>
2023-08-03 23:31:31 -0500

    dont track node_modules, updated server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const api = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api", api);
 
 app.use(express.static("public"));
+
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (error){
-            console.log(error)
+        if (err) { // Here, it should be 'err', not 'error'
+            console.log(err);
+            res.status(500).json({ error: "Unable to read the file." });
+            return;
         }
-        res.json(JSON.parse(data))
-    })
-})
+        res.json(JSON.parse(data));
+    });
+});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
